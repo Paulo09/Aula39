e3df784b1337462ccd1b020e3616820a04118ca2
Paulo09 <paullocasttro@hotmail.com>
2024-01-10 21:32:09 -0300

    Desacoplsmento

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-import HomeScreen from './src/screens/HomeScreen';
-import ProdutosScreen from './src/screens/ProdutosScreen';
-
-const Stack =  createNativeStackNavigator();
+import AppRoutes from './src/routes/AppRoutes';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-     <NavigationContainer>
-        <Stack.Navigator initialRouteName='Home'>
-            <Stack.Screen name = "Home" component={HomeScreen} />
-            <Stack.Screen name = "Produtos" component={ProdutosScreen} />
-        </Stack.Navigator>
-     </NavigationContainer>
+      <AppRoutes />
     </View>
   );
 }
@@ -25,6 +13,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
 });
